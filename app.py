dbb9aed36efe38af5a471dd3afe69812d3a50674
berhanukenea600 <berhanukenea600@gmail.com>
2026-07-19 09:50:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from PIL import Image
+model = tf.keras.models.load_model("africattles_weight_model.keras")
 import tensorflow as tf
 import numpy as np
 
@@ -24,8 +25,24 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
+
+    if "image" not in request.files:
+        return jsonify({"error": "No image uploaded"}), 400
+
+    file = request.files["image"]
+
+    img = Image.open(file).convert("RGB")
+    img = img.resize((224, 224))
+
+    img = np.array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
+
+    prediction = model.predict(img)
+
+    weight = round(float(prediction[0][0]), 1)
+
     return jsonify({
-        "predicted_weight": 180,
+        "predicted_weight": weight,
         "unit": "kg"
     })
 @app.route("/test")
