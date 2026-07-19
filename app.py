1eeacd85ccc30616a6d12d4cde8b7e747b498e30
berhanukenea600 <berhanukenea600@gmail.com>
2026-07-19 10:25:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,6 @@ def predict():
 def test():
     return jsonify({"message": "Backend works!"})
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
